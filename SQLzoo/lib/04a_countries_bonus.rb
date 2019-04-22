95073b066f20884f42464912c2e2141d8d42d9f8
patrick <patrickpondo7@gmail.com>
2019-04-22 15:56:59 -0400

    finis 4a and 6

--- SQLzoo/lib/04a_countries_bonus.rb
+++ SQLzoo/lib/04a_countries_bonus.rb
@@ -17,6 +17,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    gdp > ALL(
+      SELECT
+        COALESCE(gdp, 0)
+      FROM 
+        countries
+      WHERE
+        continent = 'Europe'  
+    );
   SQL
 end
 
@@ -24,6 +37,19 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT
+    continent, name, area
+  FROM
+    countries
+  WHERE
+    area IN (
+      SELECT
+        MAX(area)
+      FROM
+        countries
+      GROUP BY
+        continent
+    );
   SQL
 end
 
@@ -31,5 +57,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT
+    C1.name, C1.continent
+  FROM
+    countries AS C1
+  WHERE
+    population / 3 > (
+      SELECT
+        MAX(C2.population)
+      FROM
+        countries AS C2
+      WHERE
+        C1.continent = C2.continent
+        AND 
+        C1.name != C2.name
+    );
   SQL
 end
--- SQLzoo/lib/07_joins_exercises.rb
+++ SQLzoo/lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT
+      title
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'Harrison Ford';
   SQL
 end
 
@@ -48,12 +58,34 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+   SELECT
+      title
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'Harrison Ford'
+      AND ord != 1;
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+  SELECT
+      title, name
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      ord = 1
+      AND yr = 1962;
   SQL
 end
 
@@ -61,6 +93,21 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+   SELECT
+      yr, COUNT(title)
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      name = 'John Travolta'
+    GROUP BY
+      yr
+    HAVING
+      COUNT(title) >= 2;
+    
   SQL
 end
 
@@ -68,6 +115,28 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+  SELECT
+    title, name
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  WHERE
+    ord = 1
+    AND title IN (
+      SELECT
+        title
+      FROM
+        movies
+      JOIN
+        castings ON movies.id = castings.movie_id
+      JOIN
+        actors ON castings.actor_id = actors.id
+      WHERE 
+        name = 'Julie Andrews'
+    );
   SQL
 end
 
@@ -75,6 +144,18 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    movies
+  JOIN
+    castings ON movies.id = castings.movie_id
+  JOIN
+    actors ON castings.actor_id = actors.id
+  GROUP BY 
+    name
+  HAVING 
+    COUNT(ord=1) >= 15;
   SQL
 end
 
